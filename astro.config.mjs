59a1d057da18e9ba6a360dad754069fcc9a63d83
plop <realroastedchickenlegs@gmail.com>
2024-07-10 16:12:03 +0100

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+	adapter: vercel({
+		webAnalytics: true,
+	}),
+	integrations: [react(), tailwind()],
+	output: 'hybrid',
 });
