e7b57619e639235d3be1e047721357c479deaa42
plop <128096028+plop-dev@users.noreply.github.com>
2024-11-10 22:09:20 +0000

    updated projects items, updated components (#8)
    * updated underline glow, made video support autoplay
    
    * adjusted scroll trigger positions and added box shadow to progress bar; improved background color transition
    
    * added marked package for markdown parsing; updated ProjectItem component to render parsed descriptions
    
    * enhance ProjectItem and ProjectCarousel components; add newTab prop and update link behavior; configure SCSS for animations and styles

--- src/components/ui/ButtonComponent.jsx
+++ src/components/ui/ButtonComponent.jsx
@@ -3,7 +3,9 @@ import { Button } from '@/components/ui/button';
 export function ButtonComponent(props) {
 	return (
 		<Button variant={props.variant} asChild>
-			<a href={`${props.link}`}>{props.text}</a>
+			<a className='ghost' href={`${props.link}`}>
+				{props.text}
+			</a>
 		</Button>
 	);
 }
